c99a426dc4d72227515851e626e10da74fdb7d51
ShaikTanuz <142380518+ShaikTanuz@users.noreply.github.com>
2023-08-25 11:18:46 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,9 +1,4 @@
-def add_numbers(a, b):
-    return a + b
-
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-result = add_numbers(num1, num2)
-print("The sum of", num1, "and", num2, "is", result)
-
+int num1 = 7
+int num2 = 8
+int result = num1+num2
+print(" ", result)
